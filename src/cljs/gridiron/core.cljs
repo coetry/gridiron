e8ede9ddb7aedb686f3b8a0b09c8a674f518dfee
Allen Hai <coetry@Allens-MacBook-Pro.local>
2017-08-27 12:46:21 -0500

    created four-col-grid-container, more coordinate based spanning

--- src/cljs/gridiron/core.cljs
+++ src/cljs/gridiron/core.cljs
@@ -21,21 +21,32 @@
 
    [:h1 "Line-based Placement (Grid Col/Row Shorthand)"]
    
-   [g/grid-container
+   [g/three-col-grid-container
     [[g/line-based-grid-col-row-short [:p "a"] "#333333" "#FFA3D7" [2 3 1 2]]
      [g/line-based-grid-col-row-short [:p "b"] "#333333" "#9EEBCF" [2 3 2 3]]
      [g/line-based-grid-col-row-short [:p "c"] "#333333" "#96CCFF" [3 4 2 3]]
      [g/line-based-grid-col-row-short [:p "d"] "#333333" "#FFDFDF" [1 2 1 2]]
-     [g/line-based-grid-col-row-short [:p "e"] "#333333" "#555555" [1 2 2 3]]
+     [g/line-based-grid-col-row-short [:p "e"] "#333333" "#FBF1A9" [1 2 2 3]]
      [g/line-based-grid-col-row-short [:p "f"] "#333333" "#AAAAAA" [3 4 1 2]]]]
 
    [:h1 "Line-based Placement Spanning Tracks"]
    
-   [g/grid-container
+   [g/three-col-grid-container
     [[g/line-based-grid-col-row-short [:p "a"] "#333333" "#FFA3D7" [1 2 1 3]]
      [g/line-based-grid-col-row-short [:p "b"] "#333333" "#9EEBCF" [2 4 1 2]]
      [g/line-based-grid-col-row-short [:p "c"] "#333333" "#96CCFF" [2 3 2 3] ]
-     [g/line-based-grid-col-row-short [:p "d"] "#333333" "#FFDFDF" [3 4 2 3]]]]])
+     [g/line-based-grid-col-row-short [:p "d"] "#333333" "#FFDFDF" [3 4 2 3]]]]
+
+   [:h1 "More position coordinate spanning"]
+
+   [g/four-col-grid-container
+    [[g/line-based-grid-col-row-short [:p "a"] "#333333" "#FFA3D7" [1 3 1 2]]
+     [g/line-based-grid-col-row-short [:p "b"] "#333333" "#9EEBCF" [3 5 1 2]]
+     [g/line-based-grid-col-row-short [:p "c"] "#333333" "#96CCFF" [1 2 2 3]]
+     [g/line-based-grid-col-row-short [:p "d"] "#333333" "#FFDFDF" [2 5 2 3]]
+     [g/line-based-grid-col-row-short [:p "e"] "#333333" "#AAAAAA" [1 5 3 4]]]]])
+     
+    
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- src/cljs/gridiron/grids.cljs
+++ src/cljs/gridiron/grids.cljs
@@ -3,14 +3,11 @@
    [reagent.core :as r]))
 
 
-(defn header [text]
-  [:h1 {:class "header"}
-   text])
 
+(defn three-col-grid-container 
 
-(defn grid-container 
   "Takes a vector of child components
-   and makes wraps them in a grid"
+   and wraps them in a grid"
 
   [children]
   
@@ -24,7 +21,32 @@
    (for [child children]
      child)])
 
-(defn line-based-grid-col-row-short [comp color bg-color grid-pos-vec]
+(defn four-col-grid-container 
+
+  "Takes a vector of child components
+   and wraps them in a grid"
+
+  [children]
+  
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr 1fr 1fr"
+                 :grid-gap "40px 40px"
+                 :top "0"
+                 :bottom "0"
+                 :height "100vh"
+                 :margin"5% 5% 5% 5%"}}
+   (for [child children]
+     child)])
+
+
+
+(defn line-based-grid-col-row-short
+
+ "Takes a component and some styling props
+  for color and grid positioning"
+  
+  [comp color bg-color grid-pos-vec]
+
   (let [[gcs gce grs gre ] grid-pos-vec]
     [:div {:style {:text-transform "uppercase"
                    :background-color bg-color
